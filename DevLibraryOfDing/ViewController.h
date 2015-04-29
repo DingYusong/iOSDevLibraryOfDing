373370c1f9f214bbbb4fbb41dcbb19027a268a2d
丁玉松 <dys90@qq.com>
2015-04-29 15:53:37 +0800

    Initial Commit

---
+++ DevLibraryOfDing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DevLibraryOfDing
+//
+//  Created by 丁玉松 on 15/4/29.
+//  Copyright (c) 2015年 丁玉松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DevLibraryOfDing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DevLibraryOfDing
+//
+//  Created by 丁玉松 on 15/4/29.
+//  Copyright (c) 2015年 丁玉松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
